333670a8dbb765eae007e6b8310a95c5db4f5126
Navid-Radkusha <navidradkusha@gmail.com>
2021-10-29 04:38:24 +0200

    delete user, get user by id, put user, post user

--- src/main/java/com/example/shopedgeservice/controller/UserController.java
+++ src/main/java/com/example/shopedgeservice/controller/UserController.java
@@ -3,9 +3,10 @@ package com.example.shopedgeservice.controller;
 import com.example.shopedgeservice.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -23,5 +24,23 @@ public class UserController {
         User user = restTemplate.postForObject("http://" + userServiceBaseUrl +"/users/newuser", new User("Test", "test1", "navid7373", Boolean.TRUE,Boolean.FALSE,Boolean.FALSE),User.class);
 
         return user;
+    }
+
+    @DeleteMapping("/users/user/{id}")
+    public ResponseEntity deleteUser(@PathVariable String id){
+        restTemplate.delete("http://" + userServiceBaseUrl +"/users/user/"+id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/users/user/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User newUser){
+        ResponseEntity<User> userResponseEntity = restTemplate.exchange("http://" + userServiceBaseUrl +"/users/user/"+id, HttpMethod.PUT, new HttpEntity<>(newUser), User.class);
+        User retrievedUser = userResponseEntity.getBody();
+        return retrievedUser;
+    }
+
+    @GetMapping("/users/user/{id}")
+    public User getUser(@PathVariable String id){
+        return restTemplate.getForObject("http://" + userServiceBaseUrl +"/users/user/"+id, User.class);
     }
 }
